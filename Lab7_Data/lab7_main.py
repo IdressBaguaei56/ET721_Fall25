3d4dc6fd6bb37f02935e07ec7d1a0d58d1f11b9e
Idress Baguaei <Idress.Baguaei56@student.qcc.cuny.edu>
2025-10-14 11:38:27 -0400

    add lab 7

--- Lab7_Data/lab7_main.py
+++ Lab7_Data/lab7_main.py
@@ -7,5 +7,27 @@ from lab7_function import*
 
 testing()
 print("\n ----- example 1: reading file -----")
-read_data("phrases.txt", 'r') as filename:
+read_data("phrases.txt")
 
+print("\n ----- example 2: reading specific portion of a file -----")
+read_up("phrases.txt")
+
+print("\n ----- example 3: reading specific portion using readline -----")
+read_readline("phrases.txt")
+
+print("\n ----- example 4: reading specific portion using readline -----")
+read_all("phrases.txt")
+
+print("\n ----- example 5: loop through each line -----")
+read_each("phrases.txt")
+
+print("\n -----  example 6: creating a new file")
+new_file("lastname.txt")
+
+print("\n -----  example 7: append data into existing file -----")
+stamp_data("lastname.txt")
+
+print("\n ----- EXCERCISE -----")
+count_yahoo = email_read("user_email.txt", "@yahoo")
+count_gmail = email_read("user_email.txt", "@gmail")
+count_hotmail = email_read("user_email.txt", "@hotmail")
